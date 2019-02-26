e2d243886a8bd9a0d25d2826c0700ea76ab57efb
Lyubomir Kiprov <lyubo@limechain.tech>
2019-02-26 13:14:59 +0200

    Implement movie token and add tests

--- contracts/Tokens/MovieToken/MovieToken.sol
+++ contracts/Tokens/MovieToken/MovieToken.sol
@@ -5,10 +5,10 @@ import "./../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC2
 
 
 contract MovieToken is ERC20Detailed, ERC20Mintable {
-    
-    string private _name = "Mogul Movie";
-    string private _symbol = "MGLM";
-    uint8 private _decimal = 18;
 
-    constructor() ERC20Detailed(_name, _symbol, _decimal) public { }
+    constructor() ERC20Detailed(
+        "Mogul Movie Coin", // name
+        "MGLMC", // symbol
+         18 // decimals
+    ) public { }
 }
---
+++ test/movie-token-tests.js
@@ -0,0 +1,27 @@
+const etherlime = require('etherlime');
+
+const MovieToken = require('./../build/MovieToken');
+
+describe('Movie Token Contract', () => {
+
+    const OWNER = accounts[0].signer.address;
+
+    const TOKEN_NAME = 'Mogul Movie Coin'
+    const TOKEN_SYMBOL = 'MGLMC'
+    const TOKEN_DECIMALS = 18;
+
+    it('Should initialize the contract correctly', async () => {
+        let deployer = new etherlime.EtherlimeGanacheDeployer();
+        let movieTokenContract = await deployer.deploy(MovieToken, {});
+
+        let tokenName = await movieTokenContract.name();
+        let tokenSymbol = await movieTokenContract.symbol();
+        let tokenDecimals = await movieTokenContract.decimals();
+        let isMinter = await movieTokenContract.isMinter(OWNER);
+
+        assert.equal(tokenName, TOKEN_NAME, 'Token name is incorrect');
+        assert.equal(tokenSymbol, TOKEN_SYMBOL, 'Token symbol is incorrect');
+        assert.equal(tokenDecimals, TOKEN_DECIMALS, 'Token decimal is incorrect');
+        assert(isMinter, 'Owner is not a minter');
+    });
+});
