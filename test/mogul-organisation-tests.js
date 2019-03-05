2c890810eeedf1752b134652e2391832d5774c80
Lyubomir Kiprov <lyubo@limechain.tech>
2019-03-05 14:21:18 +0200

    Fix unlock function

--- test/mogul-organisation-tests.js
+++ test/mogul-organisation-tests.js
@@ -1,20 +1,26 @@
 const etherlime = require('etherlime');
-const MogulDAI = require('../build/MogulDAI');
-const MogulOrganisationTests = require('./../build/MogulOrganisation');
-const MovieToken = require('../build/MovieToken');
-const MogulToken = require('../build/MogulToken');
-const BondingMathematics = require('../build/BondingMathematics');
-const SQRT = require('./../contracts/Math/SQRT.json');
+const { buyCalc, sellCalc } = require('./utils/token-price-calculation');
 
-describe('MogulOrganisation Contract', () => {
+const MogulDAI = require('../build/MogulDAI');
+const MovieToken = require('../build/MovieToken');
+
+const SQRT = require('./../contracts/Math/SQRT.json');
+const BondingMathematics = require('../build/BondingMathematics');
+
+const MogulOrganisationTests = require('./../build/MogulOrganisation');
+
+describe.only('Mogul Organisation Contract', () => {
 
     const deployer = new etherlime.EtherlimeGanacheDeployer();
+
     const OWNER = accounts[0].signer;
+    const INVESTOR = accounts[1].signer;
+    const MOGUL_BANK = accounts[9].signer.address;
+
     const mglOrgDaiSupply = "500000000000000000";
     const initialMglSupply = "1000000000000000000";
     const ONE_ETH = "1000000000000000000";
     const TWO_ETH = "2000000000000000000";
-    const MOGUL_BANK = accounts[9].signer.address;
 
     let sqrtContractAddress;
     let bondingMathematicsInstance;
@@ -41,6 +47,11 @@ describe('MogulOrganisation Contract', () => {
             movieTokenInstance.contractAddress,
             MOGUL_BANK);
 
+
+        // Mint and Approve 1 ETH in order to unlock the organization
+        await mintDAI(OWNER.address, ONE_ETH);
+        await approveDAI(OWNER, mogulOrganisationInstance.contractAddress, ONE_ETH);
+
         await movieTokenInstance.addMinter(mogulOrganisationInstance.contractAddress);
     }
 
@@ -48,22 +59,84 @@ describe('MogulOrganisation Contract', () => {
         await mogulDAIInstance.mint(addr, amount)
     }
 
-    async function approveDAI(to, amount) {
-        await mogulDAIInstance.approve(to, amount)
+    async function approveDAI(approver, to, amount) {
+        await mogulDAIInstance.from(approver).approve(to, amount)
     }
 
-    it('Should invest', async () => {
-        await deployContracts();
-        await mintDAI(OWNER.address, TWO_ETH);
+    describe('Invest', function () {
+        it('Should invest', async () => {
+            await deployContracts();
+            await mintDAI(INVESTOR.address, ONE_ETH);
 
-        // Approve initial DAI supply in order to unlock the organization
-        await approveDAI(mogulOrganisationInstance.contractAddress, ONE_ETH);
-        await mogulOrganisationInstance.unlock(ONE_ETH);
+            // Approve 1 ETH for investment
+            await approveDAI(INVESTOR, mogulOrganisationInstance.contractAddress, ONE_ETH);
 
-        // Approve for investment
-        await approveDAI(mogulOrganisationInstance.contractAddress, ONE_ETH);
-        await mogulOrganisationInstance.invest(ONE_ETH, {
-            gasLimit: 800000
+            await mogulOrganisationInstance.unlockOrganisation(ONE_ETH);
+            await mogulOrganisationInstance.from(INVESTOR).invest(ONE_ETH, {
+                gasLimit: 300000
+            });
+
+            const EXPECTED_BANK_BALANCE = '200000000000000000'; // 0.2 ETH
+            let bankBalance = await mogulDAIInstance.balanceOf(MOGUL_BANK);
+            assert(bankBalance.eq(EXPECTED_BANK_BALANCE), 'Incorrect bank balance after investment');
+
+            const EXPECTED_RESERVE_BALANCE = '800000000000000000'; // 0.8 ETH
+            let reserveBalance = await mogulDAIInstance.balanceOf(mogulOrganisationInstance.contractAddress);
+            assert(reserveBalance.eq(EXPECTED_RESERVE_BALANCE), 'Incorrect reserve balance after investment');
+
+
+            // Wrap mogulOrganisationInstance.mogulToken in MogulToken interface
+            const EXPECTED_INVESTOR_MOGUL_BALANCE = buyCalc();
+            let investorMogulBalance = await movieTokenInstance.balanceOf(INVESTOR.address);
+            assert(investorMogulBalance.eq(EXPECTED_INVESTOR_MOGUL_BALANCE), 'Incorrect investor mogul balance after investment');
+            // let investorMovieBalance = 
+
+
+            // let daiSupply = await mogulOrganisationInstance.daiSupply();
+            // let daiReserve = await mogulOrganisationInstance.daiReserve();
+
+            // assert(daiSupply.eq('1000000000000000000'), 'Incorrect dai supply after investment');
+            // assert(daiReserve.eq());
+        });
+
+        it('Should throw if one tries to invest in non-unlocked organisation', async () => {
+            await deployContracts();
+            await mintDAI(INVESTOR.address, ONE_ETH);
+            await approveDAI(INVESTOR, mogulOrganisationInstance.contractAddress, ONE_ETH);
+
+            await assert.revert(mogulOrganisationInstance.from(INVESTOR).invest(ONE_ETH), 'An investment has been processed for a non-unlocked organisation');
+        });
+
+        it('Should throw if an investor tries to invest with unapproved DAI amount', async () => {
+            await deployContracts();
+
+            await assert.revert(mogulOrganisationInstance.from(INVESTOR).invest(ONE_ETH), 'An investment has been processed with unapproved DAI amount');
+        });
+    });
+
+    describe('Unlock', function () {
+        it('Should unlock the organisation', async () => {
+            await deployContracts();
+
+            await mogulOrganisationInstance.unlockOrganisation(ONE_ETH);
+
+            let organisationBalance = await mogulDAIInstance.balanceOf(mogulOrganisationInstance.contractAddress);
+            assert(organisationBalance.eq(ONE_ETH), 'Organisation balance is incorrect after unlocking');
+        });
+
+        it('Should throw on re-unlocking', async () => {
+            await deployContracts();
+
+            await mogulOrganisationInstance.unlockOrganisation(ONE_ETH);
+
+            await assert.revert(mogulOrganisationInstance.unlockOrganisation(ONE_ETH), 'Re-unlocking of an organisation did not throw');
+        });
+
+        it('Should throw if an unlocker tries to unlock with unapproved DAI amount', async () => {
+            await deployContracts();
+
+            // In 'deployContracts' only 1 ether is approved for unlocking
+            await assert.revert(mogulOrganisationInstance.unlockOrganisation(TWO_ETH), 'Organisation has been unlocked with unapproved DAI amount');
         });
     });
 });
