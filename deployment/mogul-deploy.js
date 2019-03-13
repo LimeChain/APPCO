cc004107632b0148fdeeb36e65e690cc49e22501
Lyubomir Kiprov <lyubo@limechain.tech>
2019-03-13 15:42:54 +0200

    Add some mogul organization setup

--- deployment/mogul-deploy.js
+++ deployment/mogul-deploy.js
@@ -11,9 +11,11 @@ const MogulOrganization = require('./../build/MogulOrganisation');
 const BondingSQRT = require('./../contracts/Math/SQRT.json');
 const TokensSQRT = require('./../contracts/Math/TokensSQRT.json');
 
+const UNLOCK_AMOUNT = '1000000000000000000'; // 1 ETH
+
 // Mogul wallet address
 let MOGUL_BANK = '0x53E63Ee92e1268919CF4757A9b1d48048C501A50';
-let DAI_TOKEN = '0xe0B206A30c778f8809c753844210c73D23001a96';
+let DAI_TOKEN_ADDRESS = '0xe0B206A30c778f8809c753844210c73D23001a96';
 
 const ENV = {
     LOCAL: 'LOCAL',
@@ -30,13 +32,19 @@ const deploy = async (network, secret) => {
 
     // Change ENV in order to deploy on test net (Ropsten)
     const deployer = getDeployer(ENV.LOCAL, secret);
+    const daiContract = await getDAIContract(deployer);
 
 
     // Deploy Movie Token
     const movieTokenContractDeployed = await deployer.deploy(MovieToken, {});
 
-    await deployMogulOrganization(deployer, movieTokenContractDeployed);
     await deployVoting(deployer, movieTokenContractDeployed);
+
+    const mogulOrganization = await deployMogulOrganization(deployer, movieTokenContractDeployed, daiContract.address);
+
+    await movieTokenContractDeployed.addMinter(mogulOrganization.contractAddress);
+    await daiContract.approve(mogulOrganization.contractAddress, UNLOCK_AMOUNT);
+    await mogulOrganization.unlockOrganisation(UNLOCK_AMOUNT);
 };
 
 let getDeployer = function (env, secret) {
@@ -48,7 +56,18 @@ let getDeployer = function (env, secret) {
     return deployer;
 }
 
-let deployMogulOrganization = async function (deployer, movieToken) {
+let getDAIContract = async function (deployer) {
+    if (deployer.ENV == ENV.LOCAL) {
+        let daiContractDeployed = await deployer.deploy(DAIToken, {});
+        await daiContractDeployed.mint(deployer.signer.address, UNLOCK_AMOUNT);
+
+        return daiContractDeployed.contract;
+    }
+
+    return new ethers.Contract(DAI_TOKEN_ADDRESS, DAIToken.abi, deployer.signer);
+}
+
+let deployMogulOrganization = async function (deployer, movieToken, daiToken) {
 
     // Deploy Organization Bonding SQRT Math
     const bondingSqrtDeployTx = await deployer.signer.sendTransaction({
@@ -64,14 +83,9 @@ let deployMogulOrganization = async function (deployer, movieToken) {
 
 
     // Deploy Organization
-
-    if (deployer.ENV == ENV.LOCAL) {
-        DAI_TOKEN = (await deployer.deploy(DAIToken, {})).contractAddress;
-    }
-
     const mogulOrganizationContractDeployed = await deployer.deploy(MogulOrganization, {},
         bondingMathContractDeployed.contractAddress,
-        DAI_TOKEN,
+        daiToken,
         movieToken.contractAddress,
         MOGUL_BANK
     );
